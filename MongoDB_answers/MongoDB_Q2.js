0d74c307fe4b9a658c33a23aa619e8676eeef129
Jonas <jonass@andrew.cmu.edu>
2025-09-30 10:38:10 -0400

    updated Q2

--- MongoDB_answers/MongoDB_Q2.js
+++ MongoDB_answers/MongoDB_Q2.js
@@ -1,13 +1,22 @@
-//Q1 What is the total revenue and the total number of orders of all time?
+//Q1 Which products made the top 3 highest revenue in September 2025?
 
 
 db.orders.aggregate([
-  { $match: { status: "paid" } },
+  { $match: {
+      status: "paid",
+      order_date: { $gte: ISODate("2025-09-01T00:00:00Z"), $lt: ISODate("2025-10-01T00:00:00Z") }
+  }},
   { $unwind: "$items" },
   { $group: {
-      _id: "$_id",
-      order_total: { $sum: { $multiply: ["$items.quantity", "$items.unit_price"] } }
+      _id: "$items.product_id",
+      revenue_usd: { $sum: { $multiply: ["$items.quantity", "$items.unit_price"] } }
   }},
-  { $group: { _id: null, orders: { $sum: 1 }, revenue_usd: { $sum: "$order_total" } } },
-  { $project: { _id: 0 } }
+  { $sort: { revenue_usd: -1 } },
+  { $limit: 3 },
+  { $lookup: {
+      from: "products", localField: "_id", foreignField: "_id", as: "prod"
+  }},
+  { $set: { name: { $first: "$prod.name" } } },
+  { $project: { _id: 0, product_id: "$_id", name: 1, revenue_usd: 1 } }
 ]);
+
